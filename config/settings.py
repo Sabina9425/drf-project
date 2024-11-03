c058475e0a22a9964d7342850987c8078235c29f
Sabina9425 <visapa@icloud.com>
2024-11-03 19:16:29 +0500

    implement viewset and generics

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dot_env = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path=dot_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
